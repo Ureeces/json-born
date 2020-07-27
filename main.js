a60a36de5a4829c890ef782687e30ed1856a83a9
Ureeces <Ulysses.Reyes@codeimmersives.com>
2020-07-27 02:45:10 -0400

    Finished getStudentByName.

--- main.js
+++ main.js
@@ -2,13 +2,22 @@ const fs = require('fs');
 const dataReadSync = fs.readFileSync('../students.json');
 const data = JSON.parse(dataReadSync);
 
-console.log(data);
+console.log(data.students);
 console.log(data.students[0]);
 
 const getStudentByIndex = function(index) {
   return data.students[index];
 }
 
+const getStudentByName = function(name) {
+  for(let student of data.students) {
+    if(student.name === name) {
+      return student;
+    }
+  }
+
+  return -1;
+}
 
 
 if (typeof getStudentByIndex === 'undefined') {
